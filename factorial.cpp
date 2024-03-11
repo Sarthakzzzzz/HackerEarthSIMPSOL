e23e6a324c6586e115ebb3417d77aef2be977612
sarthakzzzzz <144524867+Sarthakzzzzz@users.noreply.github.com>
2024-03-11 23:46:08 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int num,fact;
+	fact =1 ;
+	scanf("%d", &num);              	                
+	for (int i = 1;i<=num;i++){
+		fact = fact * i;
+
+	}
+ 	printf("%d\n",fact);   
+     return 0;
+}
